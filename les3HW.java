735287b9548cff8e8f54fe2d2b2f090673aaf26f
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2023-02-18 07:23:41 +0500

    add hw

---
+++ les3HW.java
@@ -0,0 +1,75 @@
+package HW;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class les3HW {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(58);
+        list.add(20);
+        list.add(65);
+        list.add(53);
+        list.add(74);
+        list.add(23);
+        list.add(47);
+        System.out.println(list);
+
+        removeEvenValue(list);
+        getMin(list);
+        getMax(list);
+        getAverage(list);
+
+    }
+
+    // Нужно удалить из него четные числа
+    public static List<Integer> removeEvenValue(List<Integer> list) {
+        for (int item : list) {
+            if (item % 2 == 0) {
+                list.remove(item);
+            }
+        }
+        System.out.println(list);
+        return list;
+        // return new ArrayList<>(list);
+    }
+
+    // Найти минимальное значение
+    public static Integer getMin(List<Integer> list) {
+        int minNum = list.get(0);
+        for (int i : list) {
+            if (i < minNum) {
+                minNum = i;
+            }
+        }
+        System.out.println("Минимальное значение - " + minNum);
+        return minNum;
+    }
+
+    // Найти максимальное значение
+    public static Integer getMax(List<Integer> list) {
+        int maxNum = list.get(0);
+        for (int i : list) {
+            if (i > maxNum) {
+                maxNum = i;
+            }
+        }
+        System.out.println("Максимальное значение - " + maxNum);
+        return maxNum;
+    }
+
+    // Найти среднее значение
+    public static Float getAverage(List<Integer> list) {
+        float count = 0;
+        float sum = 0;
+        float aver = 0;
+        for (int item : list) {
+            sum = sum + item;
+            count++;
+        }
+        aver = sum / count;
+        System.out.println("Среднее значение - " + aver);
+        return aver;
+    }
+    }
+
